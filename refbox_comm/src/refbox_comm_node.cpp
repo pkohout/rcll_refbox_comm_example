a7102cd9609dcbd7e60d7c0d25528f22b28a605a
Peter Kohout <peter.kohout@knapp.com>
2023-05-03 09:56:10 +0200

    update with code for receiving stuff from robot

--- refbox_comm/src/refbox_comm_node.cpp
+++ refbox_comm/src/refbox_comm_node.cpp
@@ -1,4 +1,5 @@
 #include "refbox_comm/peer.h"
+#include "refbox_comm/robot.h"
 #include <chrono>
 #include <iomanip>
 #include <sstream>
@@ -10,10 +11,10 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "refbox_comm_node");
     ros::NodeHandle n;
 
-    Peer p("localhost", 4441, 4444);
-
+    //Peer p("localhost", 4441, 4444);
+    Robot robot;
     ros::Rate r(10);
-    p.sendPrepareMachineCs();
+    //p.sendPrepareMachineCs();
     while (ros::ok()) 
     {
         ros::spinOnce();
